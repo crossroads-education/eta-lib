a6c92a85bd96b8532ba7aa9be0c7b034c0f57631
Alex Hicks <aldahick@iupui.edu>
2016-06-02 10:12:08 -0400

    Implement index.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
-export module eta {
+// *** classes ***
+export {Logger} from "./lib/classes/Logger";
 
-}
+// *** helpers ***
+export {HelperFS as fs} from "./lib/helpers/HelperFS";
+export {HelperHttp as http} from "./lib/helpers/HelperHttp";
+
+// *** interfaces ***
+export {Model} from "./lib/interfaces/Model";
+export {ModuleConfiguration} from "./lib/interfaces/ModuleConfiguration";
