8b37abd2369bc038a1b488a8f34384d064f1a04f
Alex Hicks <aldahick@iupui.edu>
2016-06-02 09:53:32 -0400

    Implement ModuleConfiguration

---
+++ lib/interfaces/ModuleConfiguration.ts
@@ -0,0 +1,22 @@
+/**
+This should never actually be defined in code.
+This is just a data structure to represent eta.json.
+*/
+export interface ModuleConfiguration {
+    /**
+    The base path a module's pages should be mounted to.
+    For example, a path of "foo" would make the module's pages accessible at "http://localhost:3000/foo/".
+    */
+    path : string;
+
+    /**
+    Optional specification of directory structure.
+    These probably shouldn't be modified, so that everything remains consistent.
+    The defaults are the key name (so the default for the "models" key is "models").
+    */
+    dirs? : {
+        models? : string;
+        static? : string;
+        views? : string;
+    };
+}
