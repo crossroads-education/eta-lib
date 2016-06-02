b7f96565a2633aa8737e01f9405677f5c72e3937
Alex Hicks <aldahick@iupui.edu>
2016-06-02 10:11:42 -0400

    Implement HelperFS
    See nodejs/node#1592 for why this is necessary

---
+++ lib/helpers/HelperFS.ts
@@ -0,0 +1,29 @@
+import "../autoload";
+
+import * as fs from "fs";
+
+export class HelperFS {
+
+    /**
+    Provides functionality of deprecated fs.exists()
+    See https://github.com/nodejs/node/issues/1592
+    */
+    public static exists(filename : string, callback : (exists : boolean) => void) : void {
+        fs.access(filename, (err : NodeJS.ErrnoException) => {
+            callback(!err);
+        });
+    }
+
+    /**
+    Provides functionality of deprecated fs.existsSync()
+    See https://github.com/nodejs/node/issues/1592
+    */
+    public static existsSync(filename : string) : boolean {
+        try {
+            fs.accessSync(filename);
+        } catch (ex) {
+            return false;
+        }
+        return true;
+    }
+}
