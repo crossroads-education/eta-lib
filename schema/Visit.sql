3b8323378eb3e447e11217fdf661a1fd4ce12ed4
Alex Hicks <aldahick@iupui.edu>
2016-06-10 13:00:21 -0400

    Fix Visit schema

--- schema/Visit.sql
+++ schema/Visit.sql
@@ -1,9 +1,8 @@
 CREATE TABLE IF NOT EXISTS `Visit` (
     `student` CHAR(32) NOT NULL,
-    `location` CHAR(2) NOT NULL,
+    `center` INT(8) NOT NULL,
     `timeIn` DATETIME NOT NULL,
     `timeOut` DATETIME,
     `section` INT(8) NOT NULL,
-    `term` CHAR(4) NOT NULL,
-    `session` CHAR(4) NOT NULL,
-PRIMARY KEY(`student`, `timeIn`, `term`));
+    `term` INT(8) NOT NULL,
+PRIMARY KEY(`student`, `timeIn`));
