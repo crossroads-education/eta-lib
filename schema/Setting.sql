345f816ef016031e5b74e12d7489d82dc154c2ec
amordel <bboy300@comcast.net>
2016-06-14 11:58:34 -0400

    Implemented setting schema

---
+++ schema/Setting.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `Setting` (
+  `page` INT(8) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(63) NOT NULL,
+  `value` TEXT NOT NULL,
+  `type` VARCHAR(15) NOT NULL,
+PRIMARY KEY(`page`, `name`));
