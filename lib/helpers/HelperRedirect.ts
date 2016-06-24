8e021e8554af61e82c3e64c879e7b2a816e7867d
Alex Hicks <aldahick@iupui.edu>
2016-06-24 09:07:12 -0400

    Fix HelperRedirect.back() because why

--- lib/helpers/HelperRedirect.ts
+++ lib/helpers/HelperRedirect.ts
@@ -6,7 +6,6 @@ export class HelperRedirect {
     public static back(req : express.Request, res : express.Response) {
         if (req.session["returnTo"]) {
             eta.logger.trace("Returning user from " + req.path + " to " + req.session["returnTo"]);
-            req.session["returnTo"] = false;
             res.redirect(req.session["returnTo"]);
         } else {
             eta.logger.trace("Returning user from " + req.path + " to root");
