18ab8f247da139cf90eee9cc002556ba3c9e297e
amordel <bboy300@comcast.net>
2016-06-29 11:20:55 -0400

    Removed history parameter and updated history schema

--- schema/History.sql
+++ schema/History.sql
@@ -1,4 +1,8 @@
 CREATE TABLE IF NOT EXISTS `History` (
     `id` INT(8) NOT NULL AUTO_INCREMENT,
-    `timestamp` DATETIME NOT NULL,
+    'user' INT(8) NOT NULL,
+    `timestamp` TIMESTAMP NOT NULL,
+    `cols` TEXT NOT NULL,
+    `params` TEXT NOT NULL,
+    `table` TEXT NOT NULL
 PRIMARY KEY(`id`));
--- schema/HistoryParameter.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS `HistoryParameter` (
-    `id` INT(8) NOT NULL,
-    `name` VARCHAR(31) NOT NULL,
-    `value` TEXT NOT NULL,
-PRIMARY KEY(`id`, `name`));
