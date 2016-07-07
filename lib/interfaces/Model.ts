a05f015f280d294dfcfa35882da567e16a47101a
Alex Hicks <aldahick@iupui.edu>
2016-07-07 14:21:09 -0400

    Add Model.renderAfter()

--- lib/interfaces/Model.ts
+++ lib/interfaces/Model.ts
@@ -6,4 +6,5 @@ import * as express from "express";
 export interface Model {
     setParams?(params : eta.ModelParams) : void;
     render(req : express.Request, res : express.Response, callback : (env : {[key : string] : any}) => void) : void;
+    renderAfter?(html : string, res : express.Response) : void;
 }
