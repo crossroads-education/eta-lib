c42009059ef0c00a85e753c4d627974a1797a137
Alex Hicks <aldahick@iupui.edu>
2016-07-08 10:14:05 -0400

    Fix History schema

--- schema/History.sql
+++ schema/History.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS `History` (
     `id` INT(8) NOT NULL AUTO_INCREMENT,
-    `user` INT(8) NOT NULL,
+    `user` CHAR(10) NOT NULL,
     `timestamp` TIMESTAMP NOT NULL,
     `cols` TEXT NOT NULL,
     `params` TEXT NOT NULL,
     `table` TEXT NOT NULL,
     `raw` TINYINT(1) NOT NULL,
+    `aux` TEXT NOT NULL,
 PRIMARY KEY(`id`));
