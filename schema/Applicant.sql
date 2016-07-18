40ae7a0dd9b686b10d2a5dd52802ec1f09643612
Alex Hicks <aldahick@iupui.edu>
2016-07-17 21:30:16 -0400

    Implement Applicant(Position) schema
    It'll probably change fairly soon - doubt I have everything here

---
+++ schema/Applicant.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `Applicant` (
+    `id` CHAR(10) NOT NULL,
+    `evaluate` TINYINT(1) NOT NULL DEFAULT 0,
+    `interview` TINYINT(1) NOT NULL DEFAULT 0,
+    `hire` TINYINT(1) NOT NULL DEFAULT 0,
+    `notes` TEXT NOT NULL DEFAULT "",
+PRIMARY KEY(`id`))
---
+++ schema/ApplicantPosition.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `ApplicantPosition` (
+    `id` CHAR(32) NOT NULL,
+    `position` VARCHAR(63) NOT NULL,
+    `lastApplied` TIMESTAMP NOT NULL,
+    `count` INT(8) NOT NULL,
+PRIMARY KEY(`id`, `position`));
