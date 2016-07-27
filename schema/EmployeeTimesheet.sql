0f6abbec7c59e0ad50f874c48743f4b3137897e4
amordel <bboy300@comcast.net>
2016-07-27 16:46:18 -0400

    Implemented employee timesheets

---
+++ schema/EmployeeTimesheet.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `EmployeeTimesheet` (
+    `id` CHAR(32) NOT NULL,
+    `ipIn` VARCHAR(45) NOT NULL,
+    `ipOut` VARCHAR(45),
+    `timeIn` DATETIME NOT NULL,
+    `timeOut` DATETIME,
+    `center` INT(8) NOT NULL
+PRIMARY KEY(`id`, `timeIn`));
