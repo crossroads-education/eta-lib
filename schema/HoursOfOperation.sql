e6669ae266ef10f18fdc75cf57c7c8bf524a6743
Alex Hicks <aldahick@iupui.edu>
2016-08-02 16:13:49 -0400

    Fix the index in HoursOfOperation schema

--- schema/HoursOfOperation.sql
+++ schema/HoursOfOperation.sql
@@ -1,8 +1,7 @@
 CREATE TABLE IF NOT EXISTS `HoursOfOperation` (
-    `id` INT(8) NOT NULL AUTO_INCREMENT,
     `term` INT(8) NOT NULL,
     `center` INT(8) NOT NULL,
-    `day` TINYINT(1) NOT NULL,
+    `day` INT(8) NOT NULL,
     `open` TIME NOT NULL,
     `close` TIME NOT NULL,
-PRIMARY KEY(`id`));
+PRIMARY KEY(`term`, `center`, `day`));
