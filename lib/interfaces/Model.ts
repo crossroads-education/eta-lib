b851b245fe515c57fc14a0f49c717b7352a260f9
Alex Hicks <aldahick@iupui.edu>
2016-08-11 08:33:52 -0400

    Necessary change for RequestHandler

--- lib/interfaces/Model.ts
+++ lib/interfaces/Model.ts
@@ -6,6 +6,7 @@ import * as express from "express";
 export interface Model {
     setParams?(params : eta.ModelParams) : void;
     onSocketIO?() : void;
+    onScheduleInit?() : void;
     render(req : express.Request, res : express.Response, callback : (env : {[key : string] : any}) => void) : void;
     renderAfter?(html : string, res : express.Response) : void;
 }
