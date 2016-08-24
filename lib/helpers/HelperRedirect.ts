0fd7383e2ffa5e861bd08da764d8596e516c3901
Alex Hicks <aldahick@iupui.edu>
2016-08-24 13:54:44 -0400

    Remove logging calls from HelperRedirect

--- lib/helpers/HelperRedirect.ts
+++ lib/helpers/HelperRedirect.ts
@@ -5,10 +5,8 @@ import * as express from "express";
 export class HelperRedirect {
     public static back(req : express.Request, res : express.Response) {
         if (req.session["returnTo"]) {
-            eta.logger.trace("Returning user from " + req.path + " to " + req.session["returnTo"]);
             res.redirect(req.session["returnTo"]);
         } else {
-            eta.logger.trace("Returning user from " + req.path + " to root");
             res.redirect("/");
         }
     }
