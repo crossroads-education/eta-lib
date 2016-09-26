d09ecb21e042213a78e5b0b3191e24b11a98de1b
Alex Hicks <aldahick@iupui.edu>
2016-09-26 19:45:18 -0400

    Add some methods to HelperPerson

--- lib/helpers/database/HelperPerson.ts
+++ lib/helpers/database/HelperPerson.ts
@@ -1,6 +1,48 @@
 import * as eta from "../../../index";
 
 export class HelperPerson {
+
+    /**
+    Returns -1 if an error occurred.
+    */
+    private static getLastNonIUID(callback : (lastID : number) => void) : void {
+        // non-IU IDs are prefixed with 'n'
+        let sql : string = `SELECT COUNT(*) AS lastID FROM Person WHERE id LIKE 'n%'`;
+        eta.db.query(sql, [], (err : eta.DBError, rows : any[]) => {
+            if (err) {
+                eta.logger.dbError(err);
+                callback(-1);
+                return;
+            }
+            callback(rows[0].lastID);
+        });
+    }
+
+    /**
+    Returns null if an error occurred.
+    */
+    public static insertNonIU(email : string, firstName : string, lastName : string, callback : (id : string) => void) : void {
+        HelperPerson.getLastNonIUID((lastID : number) => {
+            if (lastID == -1) {
+                callback(null);
+                return;
+            }
+            let sql : string = `
+                INSERT INTO Person (id, username, email, firstName, lastName)
+                VALUES(?, '', ?, ?, ?)
+                ON DUPLICATE KEY UPDATE id = VALUES(id)`;
+            let id : string = "n" + (lastID + 1);
+            eta.db.query(sql, [id, email, firstName, lastName], (err : eta.DBError, rows : any[]) => {
+                if (err) {
+                    eta.logger.dbError(err);
+                    callback(null);
+                    return;
+                }
+                callback(id);
+            });
+        });
+    }
+
     private static getInternal(moreSql : string, params : any[], callback : (person : eta.Person) => void) : void {
         let sql : string = "SELECT * FROM `Person` ";
         eta.db.query(sql + moreSql, params, (err : eta.DBError, rows : any[]) => {
@@ -27,6 +69,14 @@ export class HelperPerson {
     */
     public static getByUsername(username : string, callback : (person : eta.Person) => void) : void {
         HelperPerson.getInternal("WHERE `username` = ?", [username], callback);
+    }
+
+    /**
+    Gets a person by their unique email.
+    Returns null on failure or not found.
+    */
+    public static getByEmail(email : string, callback : (person : eta.Person) => void) : void {
+        HelperPerson.getInternal("WHERE `email` = ?", [email], callback);
     }
 
     /**
