1449813ef2ecbfe2c2efc88227fde11de7a00dcf
Alex Hicks <aldahick@iupui.edu>
2016-11-02 13:14:17 -0400

    Add OnlineRoom schema and update Center.room

--- schema/Course.sql
+++ schema/Course.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS `Course` (
     `supported` TINYINT(1) NOT NULL,
     `center` INT(8) NOT NULL,
     `tutor` VARCHAR(63),
-    `room` CHAR(4),
+    `room` CHAR(1),
     `fee` TINYINT(1) NOT NULL,
 PRIMARY KEY(`id`));
