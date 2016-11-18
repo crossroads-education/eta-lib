c9786910a247eab7bcd281d06edbab9adb6ea47c
Alex Hicks <aldahick@iupui.edu>
2016-11-18 08:58:05 -0500

    Implement HelperCenter.getLongestHoursForDay()

--- lib/helpers/database/HelperCenter.ts
+++ lib/helpers/database/HelperCenter.ts
@@ -15,6 +15,35 @@ export class HelperCenter {
         });
     }
 
+    public static getLongestHoursForDay(day: number, term: number, callback: (err: Error, hours?: eta.HoursOfOperation) => void): void {
+        let sql: string = `
+            SELECT
+                MIN(HoursOfOperation.open) AS open,
+                MAX(HoursOfOperation.close) AS close
+            FROM
+                HoursOfOperation
+            WHERE
+                HoursOfOperation.open != '00:00:00' AND
+                HoursOfOperation.close != '00:00:00' AND
+                HoursOfOperation.term = ? AND
+                HoursOfOperation.day = ?`;
+        eta.db.query(sql, [term, day], (err: eta.DBError, rows: any[]) => {
+            if (err) {
+                return callback(err);
+            }
+            if (rows.length == 0) {
+                return callback(null, null);
+            }
+            return callback(null, {
+                "term": term,
+                "day": day,
+                "open": rows[0].open,
+                "close": rows[0].close,
+                "center": -1
+            });
+        });
+    }
+
     public static getHours(id: number, term: number, callback: (err: Error, hours?: { [key: number]: eta.HoursOfOperation }) => void): void {
         let sql: string = `
             SELECT
