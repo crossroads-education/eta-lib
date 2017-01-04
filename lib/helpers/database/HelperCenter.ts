a9df655af2795ac63c76b1622e0679782ab090f9
Alex Hicks <aldahick@iupui.edu>
2017-01-04 12:38:56 -0500

    Implement HelperCenter.getLongestHoursForAllDays() and HelperCenter.getLongestHoursForWeek()

--- lib/helpers/database/HelperCenter.ts
+++ lib/helpers/database/HelperCenter.ts
@@ -44,6 +44,62 @@ export class HelperCenter {
         });
     }
 
+    public static getLongestHoursForAllDays(term: number, callback: (err: Error, hours?: { [key: number]: eta.HoursOfOperation }) => void): void {
+        let sql: string = `
+            SELECT
+                HoursOfOperation.day,
+                MIN(HoursOfOperation.open) AS open,
+                MAX(HoursOfOperation.close) AS close
+            FROM
+                HoursOfOperation
+            WHERE
+                HoursOfOperation.open != '00:00:00' AND
+                HoursOfOperation.close != '00:00:00' AND
+                HoursOfOperation.term = ?
+            GROUP BY HoursOfOperation.day`;
+        eta.db.query(sql, [term], (err: Error, rows: any[]) => {
+            if (err) {
+                return callback(err);
+            }
+            let hours: { [key: number]: eta.HoursOfOperation } = {};
+            for (let i: number = 0; i < rows.length; i++) {
+                hours[rows[i].day] = {
+                    "term": term,
+                    "day": rows[i].day,
+                    "open": rows[i].open,
+                    "close": rows[i].close,
+                    "center": -1
+                };
+            }
+            callback(null, hours);
+        });
+    }
+
+    public static getLongestHoursForWeek(term: number, callback: (err: Error, hours?: eta.HoursOfOperation) => void): void {
+        let sql: string = `
+            SELECT
+                MIN(HoursOfOperation.open) AS open,
+                MAX(HoursOfOperation.close) AS close
+            FROM
+                HoursOfOperation
+            WHERE
+                HoursOfOperation.open != '00:00:00' AND
+                HoursOfOperation.close != '00:00:00' AND
+                HoursOfOperation.term = ?`;
+        eta.db.query(sql, [term], (err: Error, rows: any[]) => {
+            if (err) {
+                return callback(err);
+            }
+            callback(null, {
+                "open": rows[0].open,
+                "close": rows[0].close,
+                "center": -1,
+                "day": -1,
+                "term": term
+            });
+        });
+    }
+
     public static getHours(id: number, term: number, callback: (err: Error, hours?: { [key: number]: eta.HoursOfOperation }) => void): void {
         let sql: string = `
             SELECT
